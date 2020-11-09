dd5857df3d36392760aab4ca4864b24b063e0057
LazyR9 <Suderman7School@outlook.com>
2020-11-08 19:09:53 -0600

    Implemented Window
    Added a resizeable and draggable window for the console to be hosted in.

---
+++ Discord Game #1/Assets/WindowDrag.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WindowDrag : MonoBehaviour, IDragHandler
+{
+    public WindowBase windowBase;
+
+    void Awake()
+    {
+        windowBase = transform.parent.GetComponent<WindowBase>();
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        windowBase.OnDrag(data);
+    }
+}
