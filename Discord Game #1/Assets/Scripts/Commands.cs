2c257c9055d70c00b0c7d56d672cd27fb156d169
LazyR9 <Suderman7School@outlook.com>
2020-11-09 17:07:18 -0600

    Implemented Console
    Got a working version of the console going, need to make more commands and make the prompt show the current user, computer, and directory. The console also does not scroll, and I will need to work on that.

--- Discord Game #1/Assets/Scripts/Commands.cs
+++ Discord Game #1/Assets/Scripts/Commands.cs
@@ -1,20 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NDesk.Options;
 
 public class Commands
 {
-    // All functions MUST return a string for an output to the console
-    
-    public string Echo(string message)
+    // Set up a reference to the console
+    public Console Console;
+    public Commands(Console console)
     {
-        return message;
+        Console = console;
+    }
+
+    // All functions MUST return a string for an output to the console.
+    // They can also use Console.WriteLine() to write to console without returning.
+    
+    public string Echo(string[] args)
+    {
+        List<string> input;
+
+        bool showHelp = false;
+
+        var options = new OptionSet()
+        {
+            {
+                "h|help",
+                "Displays the help page",
+                value => {}
+            }
+        };
+
+        try
+        {
+            input = options.Parse(args);
+        }
+        catch (OptionException e)
+        {
+            Debug.LogError(e.ToString());
+
+            Console.WriteLine("An error has occurred:");
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Use \"-h\" or \"--help\" for help");
+            return "";
+        }
+        showHelp = input.Count == 0;
+        if (showHelp)
+        {
+            StringWriter stringWriter = new StringWriter();
+            options.WriteOptionDescriptions(stringWriter);
+
+            Console.WriteLine("A basic command to test my custom command system.");
+            Console.WriteLine("Usage:");
+
+            return stringWriter.ToString();
+        }
+
+        return string.Join(" ", input);
     }
 
     public string Cd(string directory)
     {
-        GameObject.Find("Console").GetComponent<Console>().cd = directory;
+        Console.cd = directory;
         return "";
     }
-
 }
--- Discord Game #1/Assets/Scripts/Console.cs
+++ Discord Game #1/Assets/Scripts/Console.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,7 +24,6 @@ public class Console : MonoBehaviour
         {
             if (inputField.isFocused)
             {
-                Debug.Log("command sent");
                 string command = inputField.text.Replace(consoleHistory, "");
                 ProcessCommand(command);
             }
@@ -44,19 +45,31 @@ public class Console : MonoBehaviour
 
     public void ProcessCommand(string input)
     {
+        string output;
+
         string command = input.Split(' ')[0];
         List<string> args = new List<string>(input.Split(' '));
         args.RemoveAt(0);
 
-        Commands commands = new Commands();
-        //string output = (string)
-        commands.GetType().GetMethod(command).Invoke(command, args.ToArray());
+        Commands commands = new Commands(this);
+        string commandName = command.First().ToString().ToUpper() + command.Substring(1);
+        MethodInfo commandMethod = commands.GetType().GetMethod(commandName);
+        consoleHistory += input;
+        AddToConsole("\n");
+        
+        try
+        {
+            output = (string)commandMethod.Invoke(commands, new object[1]{args.ToArray()});
+        } catch (Exception e)
+        {
+            output = "An error occurred: " + e.Message;
+        }
 
-        //AddToConsole(output);
-        AddToConsole(string.Format("(user)@(pc):{0} ", cd));
+        AddToConsole(output);
+        AddToConsole(string.Format("\n(user)@(pc):{0} ", cd));
     }
 
-    public void AddToConsole(string message)
+    private void AddToConsole(string message)
     {
         inputField.text += message;
         consoleHistory += message;
@@ -69,6 +82,11 @@ public class Console : MonoBehaviour
             inputField.text = consoleHistory;
             inputField.caretPosition++;
         }
+    }
+
+    public void WriteLine(object line)
+    {
+        AddToConsole(line.ToString() + "\n");
     }
 
     void OnEnable()
