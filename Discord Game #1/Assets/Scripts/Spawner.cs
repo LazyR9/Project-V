9fea56ee28fc472ca41f467b74aab390bc689f54
LazyR9 <Suderman7School@outlook.com>
2020-11-12 15:57:31 -0600

    Update Spawner.cs
    Made it so the camera no longer "panned" to where the player spawned; now the camera spawns there as well.

--- Discord Game #1/Assets/Scripts/Spawner.cs
+++ Discord Game #1/Assets/Scripts/Spawner.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 public class Spawner : MonoBehaviour
 {
     public Transform player;
+    public Transform mainCamera;
+    public CinemachineVirtualCamera CVC;
 
     // Start is called before the first frame update
     void Start()
-    {   
+    {
         player = GameObject.Find("Player").transform;
-        player.position = this.transform.position;
+        mainCamera = GameObject.Find("CM vcam1").transform;
+        CVC = mainCamera.GetComponent<CinemachineVirtualCamera>();
+        player.position = transform.position;
+        CVC.OnTargetObjectWarped(player, player.position - mainCamera.position);
     }
 }
