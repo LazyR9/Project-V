c410e6cdc823a0d50d77983a5f99dec42d10a3dd
LazyR9 <Suderman7School@outlook.com>
2020-11-22 14:45:37 -0600

    Update PixelPerfectCamera.cs
    It will now check to see if there is a background before adjusting it; I still need to chang the way the background does adjusts though.

--- Discord Game #1/Assets/Scripts/PixelPerfectCamera.cs
+++ Discord Game #1/Assets/Scripts/PixelPerfectCamera.cs
@@ -12,9 +12,12 @@ public class PixelPerfectCamera : MonoBehaviour
         pixelPerfectCamera = mainCamera.GetComponent<UnityEngine.U2D.PixelPerfectCamera>();
         pixelPerfectCamera.refResolutionX = Screen.currentResolution.width;
         pixelPerfectCamera.refResolutionY = Screen.currentResolution.height;
-        Vector3 k = background.transform.localScale;
-        k.x = 2 * Camera.main.orthographicSize * Camera.main.aspect / background.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-        k.y = 2 * Camera.main.orthographicSize / background.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
-        background.transform.localScale = k;
+        if (background)
+        {
+            Vector3 k = background.transform.localScale;
+            k.x = 2 * Camera.main.orthographicSize * Camera.main.aspect / background.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            k.y = 2 * Camera.main.orthographicSize / background.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+            background.transform.localScale = k;
+        }
     }
 }
